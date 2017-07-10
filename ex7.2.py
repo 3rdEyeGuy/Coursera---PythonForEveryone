98519ae63ad4d851a88a173e408c769599dda638
Octavio <Oasosaf@gmail.com>
2017-07-10 00:03:39 -0500

    attempt to read float

--- ex7.2.py
+++ ex7.2.py
@@ -1,8 +1,20 @@
 # Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
 fh = open(fname)
-for line in fh:
-    if not line.startswith("X-DSPAM-Confidence:") : continue
-    print(line.rstrip())
+
+fR = fh.read()
+fR = fR.rstrip()
+value = 0
+
+for line in fR:
+    if not line.startswith("X-DSPAM-Confidence:") : continue 
+#index = line.find("0")
+print(line)
+#print(index)
+#value = float(line[index:index+6]) + value
+
+#print(value)
+
+#    print(line.rstrip())
 print("Done")
 

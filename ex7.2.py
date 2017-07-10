92582f53bd7a90d47334d553b3f03e98c3878cc2
Octavio <Oasosaf@gmail.com>
2017-07-10 00:19:05 -0500

    print average spam confidence

--- ex7.2.py
+++ ex7.2.py
@@ -1,8 +1,16 @@
 # Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
 fh = open(fname)
+value = 0
+count = 0
+
 for line in fh:
     if not line.startswith("X-DSPAM-Confidence:") : continue
-    print(line.rstrip())
-print("Done")
+    cLine = line.rstrip()
+    valIndex = cLine.find(":")
+    value = float(cLine[valIndex+2:valIndex+8]) + value
+    count = count + 1
+
+avgConf = value/count
+print("Average spam confidence:", avgConf)
 

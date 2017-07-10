fc9c591a98ce35503ef437eb9c82842be4e4fd2d
Octavio <Oasosaf@gmail.com>
2017-07-10 12:30:15 -0500

    sorts non-duplicates and prints

--- ex8.4.py
+++ ex8.4.py
@@ -1,8 +1,13 @@
 fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
+mstrLst = list()
+
 for line in fh:
     lst = line.split()
-    print(lst)
-    
-    
+    lstRng = range(len(lst))
+    for tempWrd in lst:
+        if not tempWrd in mstrLst: mstrLst.append(tempWrd)
+
+msrtSrt = mstrLst.sort()
+print(mstrLst)

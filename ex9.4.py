f41fa701aff543ca907edfac9884696a9286e0e5
Octavio <Oasosaf@gmail.com>
2017-07-11 11:49:01 -0500

    print most common email and # of occurrences

--- ex9.4.py
+++ ex9.4.py
@@ -3,11 +3,16 @@ if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
 line = list()
 addBook = dict()
+max = None
 
 for line in handle:
     if line.startswith('From '): 
-#        line.split()
-#        addBook = count(line[1]).get(line[1],0) + 1
-        print(line.split())
+        allWords = line.split()
+        words = allWords[1]
+        addBook[words] = addBook.get(words,0) + 1
     else: continue
-        
+for k,v in addBook.items():
+    if max is None or v > max:
+        max = v
+        maxWord = k
+print(maxWord, max)

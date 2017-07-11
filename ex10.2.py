49659b6414dbf923bbb69848c65c93fec9db1c3a
Octavio <Oasosaf@gmail.com>
2017-07-11 15:05:09 -0500

    print dict of hours and number of occurrences ea hour

--- ex10.2.py
+++ ex10.2.py
@@ -2,12 +2,17 @@ name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
 lin = list()
+dic = dict()
 
 for lin in handle:
     if lin.startswith('From '):
         words = lin.split()
 #        print(words)
-        tim = words[5]
-        hr = tim.split(':')
-        print(hr)
+        tim = words[5].split(':')
+        hr = str(tim[0])
+#        print(hr)
+        dic[hr] = dic.get(hr,0) + 1
+    else: continue
 
+print(dic)
+        

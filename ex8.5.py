c77ce316e8afc9e99c03fbdb28937bdd4f6db1a5
Octavio <Oasosaf@gmail.com>
2017-07-10 19:54:51 -0500

    extract emails and count number of contacts

--- ex8.5.py
+++ ex8.5.py
@@ -3,8 +3,14 @@ if len(fname) < 1 : fname = "mbox-short.txt"
 
 fh = open(fname)
 count = 0
+#lst = list()
+#lst = fh.split()
 
-
+for line in fh:
+    if line.startswith('From '):
+        line = line.split()
+        count = count + 1
+        print(line[1])
 
 
 print("There were", count, "lines in the file with From as the first word")

e6d464efd3136b3aac69057417633da90efee033
Octavio <Oasosaf@gmail.com>
2017-07-11 14:53:16 -0500

    print lists of hour, min and sec of emails

--- ex10.2.py
+++ ex10.2.py
@@ -1,4 +1,13 @@
 name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
+lin = list()
+
+for lin in handle:
+    if lin.startswith('From '):
+        words = lin.split()
+#        print(words)
+        tim = words[5]
+        hr = tim.split(':')
+        print(hr)
 

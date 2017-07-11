6d4462b14adc7be742773413535217dfd7f9cd6c
Octavio <Oasosaf@gmail.com>
2017-07-11 11:31:41 -0500

    print email lines in list form

--- ex9.4.py
+++ ex9.4.py
@@ -1,4 +1,13 @@
 name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
+line = list()
+addBook = dict()
 
+for line in handle:
+    if line.startswith('From '): 
+#        line.split()
+#        addBook = count(line[1]).get(line[1],0) + 1
+        print(line.split())
+    else: continue
+        

02d4b48afa5ee0bee5d67b85e05ecd51f667a775
Octavio <Oasosaf@gmail.com>
2017-07-11 19:29:15 -0500

    print tuple list by hour

--- ex10.2.py
+++ ex10.2.py
@@ -3,16 +3,16 @@ if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
 lin = list()
 dic = dict()
+dicLs = list()
 
 for lin in handle:
     if lin.startswith('From '):
         words = lin.split()
-#        print(words)
         tim = words[5].split(':')
         hr = str(tim[0])
-#        print(hr)
         dic[hr] = dic.get(hr,0) + 1
+        dicLs = dic.items()
     else: continue
+hrSort = sorted([(k,v) for k,v in dicLs])
 
-print(dic)
-        
+print(hrSort)       
